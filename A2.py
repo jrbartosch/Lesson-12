893e88153abd8ae1a2975144b8d270fae6ff1f70
jrbartosch <jbartosch+jed@gmail.com>
2025-01-15 08:54:45 +0600

    Add files via upload

--- A2.py
+++ A2.py
@@ -1,8 +1,6 @@
 num1 = int(input('Please Enter the Starting Number of the Range: '))
 num2 = int(input('Please Enter the Ending Number of the Range: '))
-
 print('Prime Numbers Between', num1, 'and', num2, 'are:')
-
 for num in range(num1, num2 + 1):
     if num > 1:
         for i in range(2, num):
--- A3.py
+++ A3.py
@@ -1,11 +1,9 @@
 num1 = int(input('Please Enter a Number: '))
 num2 = num1
 numlen = 0
-
 while num2 > 0:
     numlen += 1
     num2 = int(num2 / 10)
-
 if numlen >= 4:
     numlen = int(numlen / 2)
     check = 0
@@ -19,12 +17,10 @@ if numlen >= 4:
             mid2 = remainder
         num1 = int(num1 / 10)
         check += 1
-
     if mid2 == 0:
         product = mid1 * mid1
     else:
         product = mid1 * mid2
-    
     print('The Product of the Middle Digit(s) of that Number is', product)
 else:
     print('Please Enter at Least Four Digits.')
